11db5a528cd5a1e50d0e38db1a6a2eca8033ea18
Dhruv Patel <dhruvedu8@gmail.com>
2026-07-11 21:02:42 +0530

    Time: 1 ms (11.49%), Space: 19.6 MB (68.47%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return True
+            
+            ## levarage the condition where unique fails
+            if nums[low] == nums[mid] and nums[mid] == nums[high]:
+                low += 1
+                high -= 1
+                continue
+            ## First check if sorted from low to mid then proceed
+            # i.e. left side
+            if nums[low] <= nums[mid]:
+                # if yes check whether lies on the left or not
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+                
+                ## not sorted now we have to check on which side to move by checking the target and the low value ofcourse
+            # right side
+            else:
+                ## if I am on right side why would I compare left with mid, compare mid target and high to find search
+                if nums[mid] <= target <= nums[high]:
+                    ## because it exists on the right move low towards right 
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        return False
