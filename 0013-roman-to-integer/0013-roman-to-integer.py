ef42c7aff01dd5fc25be201f6ff1d99726c2b1bd
Dhruv Patel <dhruvedu8@gmail.com>
2026-08-06 22:54:59 +0530

    Time: 3 ms (79.99%), Space: 19.2 MB (88.79%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,48 +4,19 @@ class Solution:
         n = len(s)
         if n == 0:
             return 0
-        i = 0
-        while i < n:
-            ## preventing index out of bound
-            if i + 1 < n and s[i] == 'I' and s[i + 1] == 'V':
-                ans += 4
-                i += 2
-            elif i + 1 < n and s[i] == 'I' and s[i + 1] == 'X':
-                ans += 9
-                i += 2
-            elif i + 1 < n and s[i] == 'X' and s[i + 1] == 'L':
-                ans += 40
-                i += 2
-            elif i + 1 < n and s[i] == 'X' and s[i + 1] == 'C':
-                ans += 90
-                i += 2
-            elif i + 1 < n and s[i] == 'C' and s[i + 1] == 'D':
-                ans += 400
-                i += 2
-            elif i + 1 < n and s[i] == 'C' and s[i + 1] == 'M':
-                ans += 900
-                i += 2
+        roman = {
+            'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100, 'D': 500, 'M': 1000
+        }
+
+        ## checks till last to second char only, if last too is not speical, by default last char is always added else the answer will get subtracted, special means the roman 1 is less than roman 2
+        for i in range(n - 1):
+            if roman[s[i]] < roman[s[i + 1]]:
+                ans -= roman[s[i]]
+            else:
+                ans += roman[s[i]]
             
-            elif s[i] == 'I': 
-                ans += 1
-                i += 1
-            elif s[i] == 'V': 
-                ans += 5
-                i += 1
-            elif s[i] == 'X': 
-                ans += 10
-                i += 1
-            elif s[i] == 'L': 
-                ans += 50
-                i += 1
-            elif s[i] == 'C': 
-                ans += 100
-                i += 1
-            elif s[i] == 'D': 
-                ans += 500
-                i += 1
-            elif s[i] == 'M': 
-                ans += 1000
-                i += 1
-        
+        ## last char always added as it has no other value to compare with 
+        return ans + roman[s[-1]]
+            
         return ans
